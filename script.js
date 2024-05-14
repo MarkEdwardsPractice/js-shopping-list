808c67a10acca5cb6ae59c54024f1f17b1a5c1a6
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-14 15:58:51 +0900

    Add files I made to git repo

---
+++ script.js
@@ -0,0 +1,7 @@
+const addButton = document.querySelector("button");
+const input = document.querySelector("input");
+const list = document.querySelector("list");
+
+addButton.addEventListener("click", () => {
+
+});
